64d19ff71353ef72d5202358a5b605f3ba255a52
SpyLap <ballisticc@gmx.de>
2026-04-29 12:53:43 +0200

    Finalized drawing quad on the screen Quad is now being drawing using one VBO, VAO and EBO with glDrawElements().

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,8 @@
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 GLFWwindow* init();
-
+constexpr int WINDOW_WIDTH = 100;
+constexpr int WINDOW_HEIGHT = 100;
 
 int main() {
 
@@ -21,11 +22,24 @@ int main() {
         1.0f, -1.0f,
         -1.0f, -1.0f
     };
-    unsigned int VBO; glGenBuffers(1, &VBO);
+    unsigned int indices[] = {
+        0, 1, 2,
+        0, 3, 2
+    };
+    unsigned int VBO, VAO, EBO;
+    glGenBuffers(1, &VBO); glGenVertexArrays(1, &VAO); glGenBuffers(1, &EBO);
+    glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), static_cast<void *>(nullptr));
     glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0); // good practice
+
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // for clarity of drawing process
     // Render loop
     while (!glfwWindowShouldClose(window)) {
         if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
@@ -35,6 +49,8 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT);
 
         shader.use();
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -56,7 +72,7 @@ GLFWwindow* init() {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // Create window
-    GLFWwindow* window = glfwCreateWindow(50, 50, "OpenGL Window", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "OpenGL Window", nullptr, nullptr);
     if (!window) {
         std::cerr << "Failed to create GLFW window\n";
         glfwTerminate();
